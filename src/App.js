1a6a12a906fe06556f25c501fb5bda19ba298802
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-24 14:54:40 -0700

    redid images and create images component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
 
 function App() {
+  // console.log(images)
   return (
     <div className="App">
-    <h1>Test</h1>
+      <motion.div className="carousel">
+        <motion.div className="inner-carousel">
+          {/* loop over each img */}
+          {images.map((image) => {
+            return (
+              <motion.div className="item">
+                <img src={image} alt="" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './assets/1.jpeg'
+import image2 from './assets/2.jpeg'
+import image3 from './assets/3.jpeg'
+import image4 from './assets/4.jpeg'
+import image5 from './assets/5.jpeg'
+import image6 from './assets/6.jpeg'
+
+export default [image1, image2, image3, image4, image5, image6]
