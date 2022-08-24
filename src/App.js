89eb6237837f33691b236ccf009f56cbffc5fd72
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-08-24 15:44:17 -0700

    added useState and useRef for the width of the carousel

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import images from "./images";
 
 function App() {
   // console.log(images)
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    // scrollWidth - to find the whole width for when the carousel needs to stop going off infinitely
+    // offsetWidth - gives you the full width that's on the screen
+    // console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <div className="App">
-      <motion.div className="carousel">
-        <motion.div className="inner-carousel">
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
           {/* loop over each img */}
           {images.map((image) => {
             return (
-              <motion.div className="item">
+              <motion.div className="item" key={image}>
                 <img src={image} alt="" />
               </motion.div>
             );
@@ -22,6 +40,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
